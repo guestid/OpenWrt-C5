62c0ea50fb6a7d033e84d5d33b7c3f1c2b75e968
abc <abc@admin.com>
2022-03-30 09:10:12 +0800

    u

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -16,8 +16,8 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/lean/default-settings/files/zzz-default-settings
 
-# 修改主机名字 immortalwrt
-#sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/emortal/default-settings/files/99-default-settings-chinese
+# Rename hostname to OpenWrt
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
