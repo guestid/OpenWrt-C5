90e99c4c8e81e65b5f0f4609902e1f6530d258a1
guestid <guestid@163.com>
2024-01-20 16:36:18 +0800

    111

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -21,7 +21,8 @@ sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 
 # Add date version
-export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
+export DATE_VERSION="$(TZ=UTC-8 date "+%Y.%m.%d")"
+
 sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
