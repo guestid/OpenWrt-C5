9df4edc67054f7b8dd5552323402d240d833d5a5
1 <1>
2024-11-25 15:45:02 +0800

    2

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -25,7 +25,7 @@ export DATE_VERSION="$(TZ=UTC-8 date "+%Y.%m.%d")"
 sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
  
  
 
